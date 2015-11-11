cd49249bfaab107444059ee5791030bb568b9099
Feng Zheng <fzheng@codiscope.com>
2015-11-11 15:54:40 -0500

    add more in index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.get('/', function(req, res) {
 });
 
 var server = app.listen(3000, function() {
-  var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://127.0.0.1:%s', port);
 });
