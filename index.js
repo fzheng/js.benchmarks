853bbcb3955705d64d7d368a47eed9788ece6bb5
Feng Zheng <fzheng@codiscope.com>
2015-11-23 11:00:30 -0500

    update the test cases

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
-//require('./httponlysession/test.explicitly-set-httponly')(app, expSess);
-require('./httponlysession/test.missing-httponly')(app, expSess);
+require('./httponlysession/test.explicitly-set-httponly')(app, expSess);
+//require('./httponlysession/test.missing-httponly')(app, expSess);
 //require('./httponlysession/test.positive-httponly-set-to-true')(app, expSess);
 //require('./httponlysession/test.set-httponly-on-create')(app, expSess);
 //require('./httponlysession/test.set-httponly-on-non-session-cookie')(app, expSess);
 //require('./random/index')(app);
+
+//app.use(expSess({
+//  cookie: {
+//    domain: 'random.com',
+//    path: '/'
+//  }
+//}));
 
 app.disable('X-Powered-By'); // trigger X-Powered-By
 
