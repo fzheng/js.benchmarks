3747d8e29879066b27af7a95fe60f9b60e0293f8
Feng Zheng <fzheng@codiscope.com>
2015-12-04 10:46:24 -0500

    done

--- untrustedfindinput/test.find-with-untrusted-input.js
+++ untrustedfindinput/test.find-with-untrusted-input.js
@@ -6,32 +6,37 @@ module.exports = function(app) {
   'use strict';
   var db = pmongo('profile', ['profile']);
   app.post('/', function(req, res) {
-    switch(req.body.id) {
-      case 101:
-        var str_1 = "I am an example input for find.";
-        db.profile.find({name: str_1}, function(err, docs) {
-          if(!err) {
-            res.send("Postive Case" + docs);
-          }
-        });
-        break;
-      case 102:
-        db.profile.find({name: req.params.name}, function(err, docs) {
-          if(!err) {
-            res.send("Use of untrusted input" + docs);
-          }
-        });
-        break;
-      case 103:
-        var condition = "this.group == " + req.params.group;
-        db.profile.find({$where: condition}, function(err, docs) {
-          if(!err) {
-            res.send("Use of untrusted input in $where" + docs);
-          }
-        });
-        break;
-      default:
-        res.error("No default case defined");
-    }
+    db.profile.find({name: req.params.name}, function(err, docs) {
+      if(!err) {
+        res.send("Use of untrusted input" + docs);
+      }
+    });
+    //switch(req.body.id) {
+    //  case 101:
+    //    var str_1 = "I am an example input for find.";
+    //    db.profile.find({name: str_1}, function(err, docs) {
+    //      if(!err) {
+    //        res.send("Postive Case" + docs);
+    //      }
+    //    });
+    //    break;
+    //  case 102:
+    //    db.profile.find({name: req.params.name}, function(err, docs) {
+    //      if(!err) {
+    //        res.send("Use of untrusted input" + docs);
+    //      }
+    //    });
+    //    break;
+    //  case 103:
+    //    var condition = "this.group == " + req.params.group;
+    //    db.profile.find({$where: condition}, function(err, docs) {
+    //      if(!err) {
+    //        res.send("Use of untrusted input in $where" + docs);
+    //      }
+    //    });
+    //    break;
+    //  default:
+    //    res.error("No default case defined");
+    //}
   });
 };
