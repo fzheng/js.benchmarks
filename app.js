04d272c95f9e6cdc9a22b5f1c55acf3fab634bc9
Feng Zheng <fzheng@codiscope.com>
2015-12-08 17:25:55 -0500

    done

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var config = require('./config');
 var EmailService = require('./lib/email_service');
 var emailService = new EmailService(config);
 
-emailService.send(
-  {
-    from: 'summitzf@gmail.com',
-    to: ['summitzf@yahoo.com', 'summitzf@163.com'],
+emailService
+  .setFrom('summitzf@gmail.com')
+  .setTo(['summitzf@gmail.com', 'summitzf@yahoo.com'])
+  .setPayload({
     subject: 'Hello World',
     text: 'How are you doing?',
     html: '<strong>How are you doing?</strong>'
-  }
-);
+  })
+  .send(function(err, data) {
+    if(data) {
+      console.log(data);
+    }
+  });
