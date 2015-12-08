9ca8b20e89bf06aa0b92c798e0ff361143e516d0
Feng Zheng <fzheng@codiscope.com>
2015-12-08 13:48:30 -0500

    done

--- randomserver/test.negative-random-server.js
+++ randomserver/test.negative-random-server.js
@@ -3,15 +3,16 @@
 module.exports = function(app, session) {
   var min = 1;
   var max = 10;
-  var rnd1 = Math.floor(Math.random() * (max - min) + min);
+  var rnd1 = Math.floor(Math.random() * (max - min) + min - Math.random());
   app.use(session({
     sessionId: rnd1,
-    genid: function(req) {
-      return req.param.id * Math.random();
-    },
     cookie: {
       httpOnly: true,
       secure: true
     }
   }));
+
+  app.post('/', function(req, res) {
+    res.send(Math.random());
+  });
 };
