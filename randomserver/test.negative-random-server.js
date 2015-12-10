e31a24bff6d722f86603b4b234196e9f65522007
Feng Zheng <fzheng@codiscope.com>
2015-12-10 14:12:01 -0500

    done again

--- randomserver/test.negative-random-server.js
+++ randomserver/test.negative-random-server.js
@@ -1,8 +1,10 @@
-'use strict';
 
 module.exports = function(app, session) {
+  'use strict';
+
   var min = 1;
   var max = 10;
+
   var rnd1 = Math.floor(Math.random() * (max - min) + min - Math.random());
   app.use(session({
     sessionId: rnd1,
