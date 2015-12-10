724ce29faa0f092d9daf3a2fc20447854c6d361b
Feng Zheng <fzheng@codiscope.com>
2015-12-10 14:02:46 -0500

    haha

--- xss/test.xss_res_render.js
+++ xss/test.xss_res_render.js
@@ -4,12 +4,9 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     var userId = req.session.userId;
     if(userId) {
-      res.render(req.param.url);
+      res.render(userId);
     } else {
-      res.render("contributions", {
-        updateError: "Contribution percentages cannot exceed 30 %",
-        userId: userId
-      });
+      res.render(req.param.url);
     }
   });
 };
