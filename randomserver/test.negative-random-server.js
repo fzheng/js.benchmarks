6cb953cfb666686ab0d04959301e9776e0c666ab
Feng Zheng <fzheng@codiscope.com>
2016-01-22 13:08:55 -0500

    add a new vulnerability and retest

--- randomserver/test.negative-random-server.js
+++ randomserver/test.negative-random-server.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
 
   var min = 1;
   var max = 10;
-  var rnd1 = Math.floor(Math.random() * (max - min) + min - Math.random());
+  var rnd1 = Math.floor(Math.random() * (max - min) + min - Math.random() + Math.random());
 
   app.post('/negative_random_server', function(req, res) {
     req.session.sessionId = max * rnd1;
