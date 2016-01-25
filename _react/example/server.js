8c89512f6e61d49754975d5ca7b0081caa60e27f
Feng Zheng <fzheng@codiscope.com>
2016-01-25 14:07:26 -0500

    trying to create a sample react app

---
+++ _react/example/server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
+app.set('port', (process.env.PORT || 3000));
+app.disable("X-Powered-By");
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/comments', function (req, res) {
+  fs.readFile(COMMENTS_FILE, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function (req, res) {
+  fs.readFile(COMMENTS_FILE, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    let comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    const newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function (err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
+    });
+  });
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
