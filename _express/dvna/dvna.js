70f7809f2687f9a681d001c8185526345748e381
Feng Zheng <fzheng@codiscope.com>
2016-02-03 17:45:28 -0500

    refactor dvna code for static analysis

--- _express/dvna/dvna.js
+++ _express/dvna/dvna.js
@@ -13,45 +13,96 @@ var port = process.env.PORT || 3000;
 var vulnerabilities = [];
 var vulnerabilities_path = './vulnerabilities/';
 
-fs.readdir(vulnerabilities_path, function (err, folders) {
-  if (err) {
-    throw err;
-  }
+//fs.readdir(vulnerabilities_path, function (err, folders) {
+//  if (err) {
+//    throw err;
+//  }
+//
+//  DVNA.set('vulnerabilities', vulnerabilities);
+//
+//  folders.map(function (folder) {
+//    return path.join(vulnerabilities_path, folder);
+//  }).filter(function (folder) {
+//    return !fs.statSync(folder).isFile();
+//  }).forEach(function (folder) {
+//    console.log("Loading vulnerability '%s'...",  folder);
+//
+//    var vulnerability_id = path.basename(folder);
+//    var vulnerability_path = path.join(folder, 'vulnerability.js');
+//    var challenge_path = path.join(folder, 'challenge.md');
+//    var hint_path = path.join(folder, 'hint.md');
+//
+//    var vulnerability = require('./' + vulnerability_path);
+//    var challenge = fs.readFileSync(challenge_path, 'utf8');
+//    var hint = fs.readFileSync(hint_path, 'utf8');
+//
+//    vulnerability.id = vulnerability_id;
+//    vulnerability.path = vulnerability_id;
+//    vulnerability.challenge = challenge;
+//    vulnerability.hint = hint;
+//
+//    console.log("Generating challenge token for '%s'...",  vulnerability.id);
+//    vulnerability.challenge_token = challenge_token(vulnerability.id);
+//
+//    vulnerabilities.push(vulnerability);
+//
+//    if (vulnerability.server) {
+//      console.log("Mounting it in '/%s'...",  vulnerability.path);
+//      DVNA.use('/' + vulnerability.path, vulnerability.server);
+//    }
+//  });
+//});
 
-  DVNA.set('vulnerabilities', vulnerabilities);
+var vulnerability;
+DVNA.set('vulnerabilities', vulnerabilities);
 
-  folders.map(function (folder) {
-    return path.join(vulnerabilities_path, folder);
-  }).filter(function (folder) {
-    return !fs.statSync(folder).isFile();
-  }).forEach(function (folder) {
-    console.log("Loading vulnerability '%s'...",  folder);
+vulnerability = require('./vulnerabilities/command_injection/vulnerability');
+DVNA.use('/command_injection', vulnerability.server);
 
-    var vulnerability_id = path.basename(folder);
-    var vulnerability_path = path.join(folder, 'vulnerability.js');
-    var challenge_path = path.join(folder, 'challenge.md');
-    var hint_path = path.join(folder, 'hint.md');
+vulnerability = require('./vulnerabilities/cross_site_request_forgery/vulnerability');
+DVNA.use('/cross_site_request_forgery', vulnerability.server);
 
-    var vulnerability = require('./' + vulnerability_path);
-    var challenge = fs.readFileSync(challenge_path, 'utf8');
-    var hint = fs.readFileSync(hint_path, 'utf8');
+vulnerability = require('./vulnerabilities/denial_of_service/vulnerability');
+DVNA.use('/denial_of_service', vulnerability.server);
 
-    vulnerability.id = vulnerability_id;
-    vulnerability.path = vulnerability_id;
-    vulnerability.challenge = challenge;
-    vulnerability.hint = hint;
+vulnerability = require('./vulnerabilities/directory_traversal/vulnerability');
+DVNA.use('/directory_traversal', vulnerability.server);
 
-    console.log("Generating challenge token for '%s'...",  vulnerability.id);
-    vulnerability.challenge_token = challenge_token(vulnerability.id);
+vulnerability = require('./vulnerabilities/eval_remote_code_execution/vulnerability');
+DVNA.use('/eval_remote_code_execution', vulnerability.server);
 
-    vulnerabilities.push(vulnerability);
+vulnerability = require('./vulnerabilities/global_namespace_pollution/vulnerability');
+DVNA.use('/global_namespace_pollution', vulnerability.server);
 
-    if (vulnerability.server) {
-      console.log("Mounting it in '/%s'...",  vulnerability.path);
-      DVNA.use('/' + vulnerability.path, vulnerability.server);
-    }
-  });
-});
+vulnerability = require('./vulnerabilities/information_disclosure/vulnerability');
+DVNA.use('/information_disclosure', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/invalid_redirects/vulnerability');
+DVNA.use('/invalid_redirects', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/parameter_pollution/vulnerability');
+DVNA.use('/parameter_pollution', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/proto_abuse/vulnerability');
+DVNA.use('/proto_abuse', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/proto_inheritance/vulnerability');
+DVNA.use('/proto_inheritance', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/regex_dos/vulnerability');
+DVNA.use('/regex_dos', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/scope_violation/vulnerability');
+DVNA.use('/scope_violation', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/sql_injection/vulnerability');
+DVNA.use('/sql_injection', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/websocket_memory_disclosure/vulnerability');
+DVNA.use('/websocket_memory_disclosure', vulnerability.server);
+
+vulnerability = require('./vulnerabilities/xss_reflected/vulnerability');
+DVNA.use('/xss_reflected', vulnerability.server);
 
 DVNA.set('view engine', 'jade');
 DVNA.use('/assets', express.static('public'));
