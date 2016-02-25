170b8694c51a25a120327cb8e93cace38024b11d
Feng Zheng <fzheng@codiscope.com>
2016-02-25 14:21:07 -0500

    practice

--- _socketio/helloworld/index.js
+++ _socketio/helloworld/index.js
@@ -7,12 +7,34 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/chat', function (req, res) {
+  res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('request', {message: 'Hello World'});
+  socket.emit('request', {
+    from: 'socket.emit',
+    path: '/'
+  });
+  io.emit('request', {
+    from: 'io.emit',
+    path: '/'
+  });
   socket.on('response', function (data) {
     console.log(data);
+  });
+});
+
+var chat = io.of('/chat').on('connection', function (socket) {
+  socket.emit('request', {
+    from: 'socket.emit',
+    path: '/chat'
+  });
+  chat.emit('request', {
+    from: 'chat.emit',
+    path: '/chat'
   });
 });
