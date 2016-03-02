fa4f2794780a94f62d17a6e78d81aaa0c0adc2a8
Feng Zheng <fzheng@codiscope.com>
2016-03-02 15:30:33 -0500

    work on the directory listing suppress

--- _hapi/learn-hapi/server/inert.js
+++ _hapi/learn-hapi/server/inert.js
@@ -2,41 +2,80 @@
 
 const theBcrypt = require('bcrypt');
 const Joi = require('joi');
+const Path = require('path');
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  inert cases >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.register = function (server, options, next) {
-  server.route({
-    method: 'GET',
-    path: '/document1/{user}/{file}',
-    handler: function (request, reply) {
-      reply.file(path.join(request.params.user, request.params.file));
-    }
-  });
+  //server.route({
+  //  method: 'GET',
+  //  path: '/document1/{user}/{file}',
+  //  handler: function (request, reply) {
+  //    reply.file(path.join(request.params.user, request.params.file));
+  //  }
+  //});
+  //
+  //server.route({
+  //  method: 'GET',
+  //  path: '/document2/{file}',
+  //  handler: function (request, reply) {
+  //    reply.file(request.params.file);
+  //  }
+  //});
+  //
+  //server.route({
+  //  method: 'GET',
+  //  path: '/document3/{user}/{file}',
+  //  handler: {
+  //    file: function (request) {
+  //      return path.join(request.params.user, request.params.file);
+  //    }
+  //  }
+  //});
+  //
+  //server.route({
+  //  method: 'GET',
+  //  path: '/document4/{name}',
+  //  handler: {
+  //    file: function (request) {
+  //      return request.params.name;
+  //    }
+  //  }
+  //});
 
   server.route({
     method: 'GET',
-    path: '/document2/{file}',
-    handler: function (request, reply) {
-      reply.file(request.params.file);
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/document3/{user}/{file}',
+    path: '/directory/{path*}',
     handler: {
-      file: function (request) {
-        return path.join(request.params.user, request.params.file);
+      directory: {
+        path: './' // good
       }
     }
   });
 
   server.route({
     method: 'GET',
-    path: '/document4/{name}',
+    path: '/multiple/{path*}',
     handler: {
-      file: function (request) {
-        return request.params.name;
+      directory: {
+        path: [
+          './',
+          '../'
+        ],
+        listing: true // negative case
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/test/{path*}',
+    config: {
+      handler: {
+        directory: {
+          path: Path.join('.', 'directory'),
+          index: false,
+          listing: false // good
+        }
       }
     }
   });
