47952deef1ddeec77e9d1c5167825c85b7f0e348
Feng Zheng <fzheng@codiscope.com>
2016-04-01 11:35:27 -0400

    update ExpenseManager for test

--- hapi/ExpenseManager/app.js
+++ hapi/ExpenseManager/app.js
@@ -1,15 +1,15 @@
-var Hapi        = require('hapi')
-  , HapiRouter  = require('hapi-router')
-  , HapiSwagger = require('hapi-swagger')
-  , pkg         = require('./package')
-  , log         = require('winston')
-  , server;
+var Hapi = require('hapi');
+var HapiRouter = require('hapi-router');
+var HapiSwagger = require('hapi-swagger');
+var pkg = require('./package');
+var log = require('winston');
+var server;
 
 server = new Hapi.Server();
 
 server.connection({
-    host: 'localhost',
-    port: 4000
+  host: 'localhost',
+  port: 4000
 });
 
 require('./server/common/methods').init(server);  // Register server methods ## http://hapijs.com/tutorials/server-methods
@@ -18,18 +18,19 @@ require('./server/common/auth').init(server);     // Register server auth scheme
 // ## Register routes
 server.register({
   register: HapiRouter,
-  options : {
-    routesDir: __dirname + '/server/routes/'
-  },
+  options: {
+    // routesDir: __dirname + '/server/routes/'
+    routesDir: 'server/routes/'
+  }
 }, function (err) { if (err) { throw err; } });
 
 // ## Register swagger API docs generator
 server.register({
   register: HapiSwagger,
   options: {
-    basePath  : '',
-    apiVersion: pkg.version,
-  },
+    basePath: '',
+    apiVersion: pkg.version
+  }
 }, function (err) { if (err) { throw err; } });
 
 // ## Start the server if this script not loaded from some other script (tests)
